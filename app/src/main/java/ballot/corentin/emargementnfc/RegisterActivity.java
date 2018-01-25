a85d45ef8cf37c9d7db15e4f34693cecf5f80e74
Corentin Ballot <corentin.ballot@gmail.com>
2018-01-25 16:14:44 +0100

    add database usage

--- app/src/main/java/ballot/corentin/emargementnfc/RegisterActivity.java
+++ app/src/main/java/ballot/corentin/emargementnfc/RegisterActivity.java
@@ -1,9 +1,11 @@
 package ballot.corentin.emargementnfc;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.ConnectivityManager;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
@@ -18,8 +20,11 @@ import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    TextView tv_tag_id;
-    BroadcastReceiver br;
+    private TextView tv_tag_id;
+    private BroadcastReceiver br;
+
+    private SQLiteDatabase sqlitedb = null;
+    private PresenceDBHelper preDB = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +36,9 @@ public class RegisterActivity extends AppCompatActivity {
         br = new Receiver();
         IntentFilter filter = new IntentFilter("ballot.corentin.emargementnfc.NFC_TAG_ID");
         this.registerReceiver(br, filter);
+
+        preDB = new PresenceDBHelper(this);
+        sqlitedb = preDB.getWritableDatabase();
     }
 
     @Override
@@ -47,8 +55,23 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void add(View view) {
-        /* TODO: ajouter l'étudiant en base de données */
-        finish();
+        try {
+            int num_etu = Integer.parseInt(((TextView) findViewById(R.id.num_etu)).getText().toString());
+            String nom = ((TextView) findViewById(R.id.nom)).getText().toString();
+            String prenom = ((TextView) findViewById(R.id.prenom)).getText().toString();
+            String carte_id = ((TextView) findViewById(R.id.register_tag_id)).getText().toString();
+
+            // check data validity
+            if(nom.length() > 0 && prenom.length() > 0 && carte_id.length() != 20)
+                throw new Exception();
+
+            addNewEtudiant(num_etu, nom, prenom, carte_id);
+
+            Toast.makeText(this, "Etudiant ajouté", Toast.LENGTH_SHORT).show();
+            finish();
+        } catch (Exception e) {
+            Toast.makeText(this, "Erreur lors de l'ajout", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public class Receiver extends BroadcastReceiver {
@@ -57,5 +80,15 @@ public class RegisterActivity extends AppCompatActivity {
         public void onReceive(Context context, Intent intent) {
             tv_tag_id.setText(intent.getExtras().getString("id"));
         }
+    }
+
+    public boolean addNewEtudiant(int numEtud, String name, String surname, String carte){
+        ContentValues cv = new ContentValues();
+        cv.put(PresenceDBHelper.ETUDIANT_NAME, name);
+        cv.put(PresenceDBHelper.ETUDIANT_SURNAME, surname);
+        cv.put(PresenceDBHelper.ETUDIANT_NUM_CARTE, carte);
+        cv.put(PresenceDBHelper.ETUDIANT_ID, numEtud);
+        sqlitedb.insert(PresenceDBHelper.ETUDIANT_TABLE_NAME, null, cv);
+        return true;
     }
 }
