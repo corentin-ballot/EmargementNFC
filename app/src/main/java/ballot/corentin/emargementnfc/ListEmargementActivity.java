0e818881c92dc562070c80f7c263db1a3072e8cc
Corentin Ballot <corentin.ballot@gmail.com>
2018-01-25 14:54:10 +0100

    remove promotion field

---
+++ app/src/main/java/ballot/corentin/emargementnfc/ListEmargementActivity.java
@@ -0,0 +1,13 @@
+package ballot.corentin.emargementnfc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListEmargementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_emargement);
+    }
+}
